d35fb7ef00523fcccedc7e4a61cbfa801ff64579
lakshitha srimal <e17338@eng.pdn.ac.lk>
2021-08-19 14:04:42 +0530

    path changes

--- WebApp/front end/src/App.js
+++ WebApp/front end/src/App.js
@@ -3,19 +3,6 @@ import { Link } from 'react-router-dom';
 import Template from './asserts/pages/template';
 import Logo from './asserts/pages/logo';
 
-function Page() {
-  return (
-    <div className="row">
-      <div className="col-6">
-        <div className="list-group" id="list-tab" role="tablist">
-            Home
-           <Link className="list-group-item list-group-item-action" to='/Login'> Login </Link>
-           <Link className="list-group-item list-group-item-action" to='/Register'> Register </Link>
-       </div>
-      </div>
-    </div>
-  );
-}
 
 export default function App() {
 
@@ -25,6 +12,23 @@ export default function App() {
   return (
     <div className="App">
          <Template left={logo} right={page} />
+    </div>
+  );
+}
+
+///page content
+function Page() {
+  return (
+    <div className="row">
+      <div className="col-6">
+        <div className="list-group" id="list-tab" role="tablist">
+            Home
+           <Link className="list-group-item list-group-item-action" to='/Login'> Login </Link>
+           <Link className="list-group-item list-group-item-action" to='/Register'> Register </Link>
+           <Link className="list-group-item list-group-item-action" to='/Dashboard/profile'> Dashboard </Link>
+           <Link className="list-group-item list-group-item-action" to='/Admin/profile'> Admin-panel </Link>       
+        </div>
+      </div>
     </div>
   );
 }
