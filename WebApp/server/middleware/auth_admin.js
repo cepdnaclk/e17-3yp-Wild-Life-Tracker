e1620ae945f310d84bb12e8cddb4a8e2eed847b1
Esara Sithumal <wmesithumal2@gmail.com>
2021-09-08 16:27:42 +0530

    implement admin authorization middleware

--- WebApp/server/middleware/auth_admin.js
+++ WebApp/server/middleware/auth_admin.js
@@ -1,15 +1,15 @@
-//auth middleware
-//check our endpoints with express validator (for pages with signin permission)
+//admin_auth middleware
+//check our endpoints with express validator (for pages with signin permission only fro admins)
 
 const jwt = require('jsonwebtoken')
 
 module.exports = (req,res,next) => {
     try{
         const token = req.header('x-auth-token')
-        //console.log(token)
         jwt.verify(token, "longer-secret-is-better")
         const decodedToken = jwt.decode(token);
         req.userEmail = decodedToken.email;
+        //if isAdmin field is not in the token, it is not a admin token
         if(!decodedToken.isAdmin){
            return res.status(401).json({
                 message: "Not an Admin, cannot Authorize",
