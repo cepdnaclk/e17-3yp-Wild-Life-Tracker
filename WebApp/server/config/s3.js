2b2c121a3ef1a68d55b032b249299b825bc70e4e
hansa31 <hansaalahakoon@gmail.com>
2021-10-14 23:43:59 +0530

    Tried to connect s3 and failed

--- WebApp/server/config/s3.js
+++ WebApp/server/config/s3.js
@@ -1,12 +1,13 @@
 //AWS s3 buckets confgiuration
 
 require('dotenv').config()
+const fs = require('fs')
 const S3 = require('aws-sdk/clients/s3')
 
 const bucketName = process.env.AWS_BUCKET_NAME
 const region = process.env.AWS_BUCKET_REGION
 const accessKeyId = process.env.AWS_ACCESS_KEY
-const secretAccesskey = process.env.AWS_SECRET_key
+const secretAccesskey = process.env.AWS_SECRET_KEY
 
 const s3 = new S3({
     region,
@@ -14,4 +15,14 @@ const s3 = new S3({
     secretAccesskey
 })
 
-module.exports = s3
+//downloads a file from S3
+function getFileStream(fieldKey){
+    const downloadParams = {
+        Key: fieldKey,
+        Bucket: bucketName
+    }
+
+    return s3.getObject(downloadParams).createReadStream()
+
+}
+exports.getFileStream = getFileStream
