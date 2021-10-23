789476887d33ca132ffc8f451a666bc13a89a776
lakshitha srimal <e17338@eng.pdn.ac.lk>
2021-10-24 05:05:08 +0530

    locations-done

--- WebApp/front end/src/asserts/pages/maps/SimpleMap.jsx
+++ WebApp/front end/src/asserts/pages/maps/SimpleMap.jsx
@@ -1,40 +1,127 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import axios from 'axios';
+import Cookies from 'universal-cookie';
+import ReactDOM from 'react-dom';
 import './map.css';
 import Zoom from 'react-reveal/Zoom';
-const positions = [[6.416809206663523, 81.35046167593366],
-                  [6.420562083627918, 81.34805841681332],
-                  [6.38132609674358, 81.38445062634995],
-                  [6.3929266152799205, 81.41912622222918],
-                  [6.403162148410246, 81.48504418667288],
-                  [6.3291205194141655, 81.42736596778464],
-                  [6.323319573216925, 81.46547479097866]]; 
+import Loader from "react-loader-spinner";
 
-const listItems = positions.map((val, index) => 
-        <Marker position={val}>
-          <Popup>
-            Device {index+1}
-          </Popup>
-        </Marker>);
+/*cookies*/
+const cookies = new Cookies();
+
+
+/*backend url*/
+const URL = process.env.REACT_APP_BACKEND_URL;
+
+var device_list = [];
 
 export default function SimpleMap() {
+
+  const location_list = [];
+
+  function fetchStationsData() {
+    axios.get(`${URL}api/auth/devices_list`, {
+        headers: {
+          'x-auth-token' : cookies.get('token')
+        }
+      })
+    
+        .then(function (response) {
+          
+          cookies.set('newtoken', response.data.token , { path: '/' });
+          device_list = response.data.deviceArray;
+
+          for(var i=0 ; i<device_list.length ; i++){
+
+            axios.get(`${URL}api/auth/device_photos/${i}`, {
+              headers: {
+                'x-auth-token' : cookies.get('newtoken')
+              }
+            })
         
-return(
+              .then(function (response) {
+                
+                  var loc = response.data.device.location;
+                  var temp = [loc.longitude,loc.latitude ];
+                  location_list.push(temp);
+                       
+              })
+  
+            .catch(function (error) {
+        
+                console.log(error);
+                //cookies.set('user', admin , { path: '/' });
+                //document.getElementById('requests-list').innerHTML = "!!!Something went to wrong. Plesse try again later!!!";
+                //document.getElementById('requests-list').className ='token-error';
+            });
+
+      }
+
+      
+      })
+  
+        .catch(function (error) {
+        console.log(error);
+          //cookies.set('user', admin , { path: '/' });
+          //document.getElementById('requests-list').innerHTML = "!!!Something went to wrong. Plesse try again later!!!";
+          //document.getElementById('requests-list').className ='token-error';
+        });
+      
+       setTimeout(() => {  
+
+        if(document.getElementById('map-loader')!==null && document.getElementById('map-container')!==null){
+                    document.getElementById('map-loader').style.display='none';
+                    ReactDOM.render(<MapUl data={location_list}/>, document.getElementById('map-container'));}
+
+      }, 3000);
+
+  }
+    
+  return(
+    <div>
+      <div id='map-container' onLoad={fetchStationsData()}>
+      </div>
+      <div className="offset-5 col-2 text-center" id='map-loader'>
+        <Loader
+          type="Puff"
+          color=" #188459"
+          height={100}
+          width={100}
+        />
+        <h3>Please Wait....</h3>
+      </div>
+    </div>
+    );
+
+  
+}
+
+function MapUl(props){
+
+  const arr = props.data;
+  const listItems = arr.map((val, index) => 
+    <Marker position={val}>
+      <Popup>
+        Device {device_list[index]}
+      </Popup>
+    </Marker>);
+  
+  return(
+  
   <div>
     <Zoom>
-  <MapContainer center={positions[0]} zoom={10} scrollWheelZoom={true}>
-    <TileLayer
-      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-
-    {listItems}
-
-  </MapContainer>
-  </Zoom>
+      <MapContainer center={arr[0]} zoom={9.5} scrollWheelZoom={true}>
+    
+        <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {listItems}
+      </MapContainer>
+    </Zoom>
   </div>
-);
-  
+  );
 }
 
 
